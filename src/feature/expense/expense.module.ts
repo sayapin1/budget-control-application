f99b461367cab3496a9f012039d46b40be74e3ab
sayapin1 <sayapinn1@gmail.com>
2023-11-15 19:53:16 +0900

    [#3] add: monthlyExpense.entity.ts 파일 추가

--- src/feature/expense/expense.module.ts
+++ src/feature/expense/expense.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { Expense } from '../../entity/expense.entity';
+import { MonthlyExpense } from '../../entity/monthlyExpense.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExpenseController } from './expense.controller';
 import { ExpenseService } from './expense.service';
 import { ExpenseLib } from './expenseLib';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Expense])],
+  imports: [TypeOrmModule.forFeature([Expense, MonthlyExpense])],
   controllers: [ExpenseController],
   providers: [ExpenseService, ExpenseLib],
   exports: [ExpenseLib],
